0d9a6e7f9ae13f558ca922b8d4aafae71f5cf24a
ETHreds <jcmaglaw@gmail.com>
2023-10-03 12:00:37 +0300

    Iterator to string

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function* createIteratorObject(report) {
+  const { allEmployees } = report;
+  for (const department in allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(allEmployees, department)) {
+      for (const employee of allEmployees[department]) {
+        yield employee;
+      }
+    }
+  }
+}
