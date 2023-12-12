20eda27157e79ccc082a92ecc12d6370b5bd0d91
ETHreds <jcmaglaw@gmail.com>
2023-12-12 12:40:25 +0300

    lint fixed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,9 @@
+/* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  promise = promise.then((response) => {
-    return {status: 200, body: "success"}
-  });
-  promise = promise.catch((error) => {
-    return new Error();
-  });
+  promise = promise.then((response) => ({ status: 200, body: 'success' }));
+  promise = promise.catch((error) => new Error());
   promise = promise.finally(() => {
     console.log('Got a response from the API');
-  })
+  });
   return promise;
 }
