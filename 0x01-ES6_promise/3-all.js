cb5f38ffa012fab49be8f517c5f3d3e002730844
Joe Magolo <104914856+ETHreds@users.noreply.github.com>
2024-10-01 12:41:18 -0700

    Resolve data from api

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { createUser, uploadPhoto } from "./utils"
+
+export default function handleProfileSignup(){
+
+    const profilePhoto = uploadPhoto()
+    const userName = createUser()
+
+    return Promise.all([profilePhoto, userName]).then((result) => {
+        console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`)
+    }).catch(() => (console.error('Signup system offline')))
+}
